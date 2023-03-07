52a55e0a9c088883b3d9f148a878e4464a73a0b7
ravneet998 <109993408+ravneet998@users.noreply.github.com>
2023-03-06 22:15:23 -0500

    Delete Back end part 2  directory

--- Back end part 2 /src/db/seeds/02-ticket.js
+++
@@ -1,7 +0,0 @@
-const tickets = require("./02-ticket.json");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE ticket RESTART IDENTITY CASCADE")
-    .then(() => knex("ticket").insert(tickets));
-};
